d7da51f55f7cfa64f95fb6052ca216bc402db311
Account for Work <zsv.main@gmail.com>
2021-02-08 06:40:49 +0500

    feat: add request to flickr

---
+++ src/store/utils/credentials.js
@@ -0,0 +1,8 @@
+const flickr = {
+    url: 'https://www.flickr.com/services/rest/',
+    method: '?method=flickr.photos.search',
+    apiKey: '&api_key=4a8fb46ba682295d6d591fb77d006cc0',
+    mode: '&tag_mode=all&extras=url_m&format=json&nojsoncallback=1&tags='
+}
+
+export {flickr}
--- src/store/utils/services.js
+++ src/store/utils/services.js
@@ -1,5 +1,6 @@
 import list from '../utils/listBirds';
 import listBirds from '../utils/listBirds'
+import {flickr} from '../utils/credentials'
 
 const rebirds = (objBirds) => {
     let idx = Math.floor(Math.random()*6);
@@ -16,13 +17,23 @@ const rebirds = (objBirds) => {
 }
 const birds  = rebirds(listBirds);
 const photoBird = (birds) => {
-    const answer = null;
+    let answer = null;
     birds.forEach(element => {
         if (element[2] === 'answer') {
             answer = element;
         }
     });
+    return answer;
+}
+const photo = photoBird(birds);
+const photoRequest = async (photoBird) => {
+    const {url, method , apiKey, mode} = flickr;
+    const urlFetch = url+method+apiKey+mode+photoBird[1];
+    debugger
+    await fetch(urlFetch).then((response)=> response.json()).then((result)=>console.log(result))
 }
 
 
+console.log(photoRequest(photo))
+debugger
 export default birds;
